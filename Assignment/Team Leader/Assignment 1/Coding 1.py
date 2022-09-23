78c57b6483e38a72167c26adfe70d113510ba141
Susi143 <112759719+Susi143@users.noreply.github.com>
2022-09-23 14:48:31 +0530

    Add files via upload

---
+++ Assignment/Team Leader/Assignment 1/Coding 1.py
@@ -0,0 +1,54 @@
+
+
+lista=[1,2,3,4,5]
+print(lista)
+
+#insertion
+lista.insert(5,10)
+print(lista)
+
+#deletion of first occurence of integer
+del lista[0]
+print(lista)
+
+#insertion at end of lista
+lista.append(-5)
+print(lista)
+
+#sorting
+lista.sort()
+print(lista)
+
+#popping last element
+lista.pop()
+print(lista)
+
+#reverse the lista
+lista.reverse()
+print(lista)
+
+#STRING OPERATIONS:
+
+#concatenate string
+stra=("Hello")
+strb=("World")
+print(stra+strb)
+
+#reverse of String
+strc="Education"
+print(strc[::-1])
+
+#slice
+print(strc[slice(1,5,2)])
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment/Team Leader/Assignment 1/Output2.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:\Users\sushm\Downloads\coding 2.py ================
+please select operation -
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+
+select operations from 1, 2, 3, 4 :3
+Enter first number: 765
+Enter second number: 356
+765 * 356 = 272340
