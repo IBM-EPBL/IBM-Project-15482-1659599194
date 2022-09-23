6d00747c9b037cf7395f526706ab6b91dcfc9b2f
Sowmya11258 <113896389+Sowmya11258@users.noreply.github.com>
2022-09-23 15:31:34 +0530

    Add files via upload

---
+++ Assignment/Team Member 3/Assignment 1/Output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:\Users\sushm\Downloads\coding 1.py ================
+[4, 5, 8, 9, 2]
+[4, 5, 8, 9, 2, 11]
+[5, 8, 9, 2, 11]
+[5, 8, 9, 2, 11, -5]
+[-5, 2, 5, 8, 9, 11]
+[-5, 2, 5, 8, 9]
+[9, 8, 5, 2, -5]
+HelloWorld
+noitacudE
+dc
---
+++ Assignment/Team Member 3/Assignment 1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[4,5,8,9,2]
+print(list)
+
+#insertion
+list.insert(9,11)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(-5)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+strx=("Hello")
+stry=("World")
+print(strx+stry)
+
+#reverse of String
+strz="Education"
+print(strz[::-1])
+
+#slice
+print(strz[slice(1,5,2)])
+
+
+
+
+
+
+
+
+
+
+
+
