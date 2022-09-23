dfa52eb16037b609a9e1b47fbbd515639b5895f0
Narmadha-J <112760721+Narmadha-J@users.noreply.github.com>
2022-09-23 14:56:49 +0530

    Add files via upload

---
+++ Team Member 1/Assignment 1/coding 2 output.py
@@ -0,0 +1,15 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:\Users\Aruna\Downloads\coding 2.py ================
+please select operation -
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+
+select operations from 1, 2, 3, 4 :3
+Enter first number: 87
+Enter second number: 2
+87 * 2 = 174
+
