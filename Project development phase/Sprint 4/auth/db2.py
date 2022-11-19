103cb1e5e715bdda0dc3459a3257b3df58ffeebb
Susi143 <112759719+Susi143@users.noreply.github.com>
2022-11-19 22:31:10 +0530

    Delete db2.py

--- Project development phase/Sprint 4/auth/db2.py
+++
@@ -1,6 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-cred = credentials.Certificate('./auth/serviceAccountKey.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
